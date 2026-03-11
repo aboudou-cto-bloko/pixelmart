db77ee8c7c53d7c81b42c27691024383f629da52
Aboudou ZINSOU <aboudou@bloko.me>
2026-03-11 01:12:01 +0100

    shit fuck again again

--- src/app/(storefront)/layout.tsx
+++ src/app/(storefront)/layout.tsx
@@ -9,7 +9,10 @@ export default function StorefrontLayout({
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      <main className="flex-1 items-center">{children}</main>
+      <main className="flex-1">
+        {/* Conteneur centré */}
+        <div className="container mx-auto px-4 py-6">{children}</div>
+      </main>
       <Footer />
     </div>
   );
